25e96922d9c79ceda646b30570b0a8b13c47f8d4
Mamad <mohammadtt77@yahoo.com>
2023-11-27 21:29:08 +0330

    add React before 18

--- 01-pizza-menu/pizza-menu/src/index.js
+++ 01-pizza-menu/pizza-menu/src/index.js
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello bitch</h1>;
+  return (
+    <h1>
+      Hello bitch
+      <Dick />
+    </h1>
+  );
 }
 
+function Dick() {
+  return <h1>kir to in net </h1>;
+}
+
+// React V18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* strictMode is a mode for development */}
     <App />
   </React.StrictMode>
 );
+
+// React Before 18
+// React.render(<App/>)
