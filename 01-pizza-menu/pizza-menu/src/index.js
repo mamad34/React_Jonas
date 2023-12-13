905af24e1895810bec98f6b4d332e0f40a98a08b
Mamad <mohammadtt77@yahoo.com>
2023-12-13 22:04:36 +0330

    Add extra components called Orded

--- 01-pizza-menu/pizza-menu/src/index.js
+++ 01-pizza-menu/pizza-menu/src/index.js
@@ -155,10 +155,7 @@ function Footer() {
         </div>
       )} */}
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or orded online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00. {closeHour}:00.{" "}
@@ -167,6 +164,17 @@ function Footer() {
     </footer>
   );
   // if the isOpen is true then <p>Open</p> will shown
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or orded online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 // React V18
