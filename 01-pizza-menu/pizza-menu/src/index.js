6a6883e11dc0fc05639146de3fe62b7dd2ee1b71
Mamad <mohammadtt77@yahoo.com>
2023-12-24 14:31:22 +0330

    Add SOLD OUT

--- 01-pizza-menu/pizza-menu/src/index.js
+++ 01-pizza-menu/pizza-menu/src/index.js
@@ -122,16 +122,21 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) {
-    return null;
-  }
+  // if (pizzaObj.soldOut) {
+  //   return null;
+  // }
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients} </p>
-        <span>{pizzaObj.price + 1}</span>
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price + 1}</span>
       </div>
     </li>
   );
