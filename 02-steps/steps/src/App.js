097270525d346f5eb10688efdec3bc59c8ee0ffa
Mamad <mohammadtt77@yahoo.com>
2024-01-02 19:46:25 +0330

    Add * button

--- 02-steps/steps/src/App.js
+++ 02-steps/steps/src/App.js
@@ -8,6 +8,8 @@ const messages = [
 export default function App() {
   let [step, setStep] = useState(1);
   const [test, setTest] = useState({ name: "Mamad" });
+  const [isOpen, setIsOpen] = useState(true);
+
   function handlePrevious() {
     if (step > 1) setStep((step -= 1));
     // equal to step - 1
@@ -23,33 +25,40 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step === 1 ? "active" : ""}>1</div>
-        <div className={step === 2 ? "active" : ""}>2</div>
-        <div className={step === 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step} {messages[step - 1]}
-        {test.name}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-          // onMouseEnter={() => {
-          //   alert("kos mikham");
-          // }}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} {messages[step - 1]}
+            {test.name}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+              // onMouseEnter={() => {
+              //   alert("kos mikham");
+              // }}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
