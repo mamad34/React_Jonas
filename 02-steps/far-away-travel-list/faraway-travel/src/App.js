8f263e2b16c801997674ca118baaab9eb657e683
Mamad <mohammadtt77@yahoo.com>
2024-01-07 00:12:53 +0330

    Add layout

--- 02-steps/far-away-travel-list/faraway-travel/src/App.js
+++ 02-steps/far-away-travel-list/faraway-travel/src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <h1>Mamad</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>Far Away 😍</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😁💕 trip ?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have X items on your list , and you already packed X (x%) 🫠</em>
+    </footer>
+  );
 }
