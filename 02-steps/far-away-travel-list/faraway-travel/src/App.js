bcc83c26814d05b3ebaa31de07cf38e5beb7f395
mamad <mohammadtt77@yahoo.com>
2024-01-17 19:31:13 +0330

    Add sorting items

--- 02-steps/far-away-travel-list/faraway-travel/src/App.js
+++ 02-steps/far-away-travel-list/faraway-travel/src/App.js
@@ -76,10 +76,28 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -88,6 +106,13 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by the input order</option>
+          <option value="description">Sort by the description</option>
+          <option value="packed">Sort by the packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
