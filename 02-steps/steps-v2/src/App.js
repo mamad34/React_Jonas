ac445958cae0c14e5d67365cad054eff077f6abb
Mamad <mohammadtt77@yahoo.com>
2024-01-22 02:26:51 +0330

    Add step-v2 and children prop

--- 02-steps/steps-v2/src/App.js
+++ 02-steps/steps-v2/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
+      {/* <Steps /> */}
     </div>
   );
 }
@@ -64,24 +64,28 @@ function Steps() {
             {test.name}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-              // onMouseEnter={() => {
-              //   alert("k mikham");
-              // }}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>⏮️</span>Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>⏭️</span>
+              {/* between opening and closing tag called childern prop */}
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+      {/* children is reserved word */}
+    </button>
   );
 }
