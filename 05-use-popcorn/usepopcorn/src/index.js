152b197ff9ccf4378470a6dc0a9bb9c64578073c
Mamad <mohammadtt77@yahoo.com>
2024-03-09 21:04:19 +0330

    Add improving reusability with props

--- 05-use-popcorn/usepopcorn/src/index.js
+++ 05-use-popcorn/usepopcorn/src/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
+      <p>This Movies Was Rated {movieRating} Stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Test />
+    <StarRating size={24} className="test" defaultRating={3} />
   </React.StrictMode>
 );
